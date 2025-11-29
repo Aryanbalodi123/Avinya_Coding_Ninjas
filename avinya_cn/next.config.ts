681dc033e43e90acf0d3971b91d2e800a1070a49
Aryanbalodi123 <aryanb3244@gmail.com>
2025-11-29 11:17:43 +0530

    bug

--- avinya_cn/next.config.ts
+++ avinya_cn/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig = {
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
   },
+  experimental: {
+    appDir: true,   // <<< THIS IS REQUIRED
+  },
 };
 
 module.exports = nextConfig;
